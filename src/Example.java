d99f98af2cab498472bb93ac0420e0f9355d06b0
Lavenababu <lavenababu@yahoo.com>
2021-09-07 15:09:06 +0530

    Variables Assigned

---
+++ src/Example.java
@@ -0,0 +1,11 @@
+/*
+    This is my First Java Program
+ */
+class Example {
+    public static void main(String[] arg) {
+        int num = 10;
+        System.out.println("num = "+num);
+        num = 2 * num;
+        System.out.println(num);
+    }
+}
