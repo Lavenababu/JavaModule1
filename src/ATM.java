b3feee8db7dc70952ee956c479adc9f85604d0ce
Lavenababu <lavenababu@yahoo.com>
2021-09-14 15:55:34 +0530

    Bikers Qualified

---
+++ src/ATM.java
@@ -0,0 +1,19 @@
+/*
+Pooja would like to withdraw X $US from an ATM.
+The cash machine will only accept the transaction if X is a multiple of 5,
+and Pooja's account balance has enough cash to perform the withdrawal transaction (including bank charges).
+For each successful withdrawal the bank charges 0.50 $US.
+Calculate Pooja's account balance after an attempted transaction.
+ */
+
+public class ATM {
+    public static void main(String[] args) {
+
+        double amt_to_withdraw = 20, acc_balance = 1000;
+
+        System.out.println();
+
+
+
+    }
+}
