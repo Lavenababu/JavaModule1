cc77acb7422234a4285dcd2461bd764d3ad5e645
Lavenababu <lavenababu@yahoo.com>
2021-09-14 14:49:00 +0530

    Calculating salary

--- src/Example.java
+++ src/Example.java
@@ -4,7 +4,7 @@
 class Example {
     public static void main(String[] arg) {
         int num = 10;
-        System.out.println("num = "+num);
+        System.out.println("num = " + num);
         num = 2 * num;
         System.out.println(num);
     }
